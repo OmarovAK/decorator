f9f34e1cfc830c22e8ae469d4ffde567f053aa1f
OmarovAK <Omarov_Anuar@mail.ru>
2022-07-24 03:01:24 +0545

    modify main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def converter(temp_count, temp_name):
         temp_count = input('Введите правильное число: ')
     while temp_name not in ['c', 'C', 'f', 'F']:
         temp_name = input('Введите правильное наименование: ')
-    if temp_name == 'c' or 'C':
-        farengaite = f'{int(temp_count) * 9 / 5 + 32:.5f}'
+    if temp_name in ['c', 'C']:
+        farengaite = f'{int(temp_count) * 9 / 5 + 32:.2f}'
         result = f'Температура по Фаренгейту равняется: {farengaite} градусов'
         print(result)
         return result
     else:
-        celciy = f'{(int(temp_count) - 32) / 9 * 5:.5f}'
+        celciy = f'{(int(temp_count) - 32) / 9 * 5:.2f}'
         result = f'Температура по Цельсию равняется: {celciy} градусов'
         print(result)
         return result
